3b5477bb2b2867f58fa358b6dbf68113c3bdf6ac
Yash Kumar Jha <amitjha329@gmail.com>
2024-05-08 14:01:49 +0530

    updated replica set script

--- scripts/replica-init.js
+++ scripts/replica-init.js
@@ -3,19 +3,23 @@ rs.initiate({
     members: [
         {
             _id: 0,
-            host: "mongodb-primary:27017"
+            host: "mongodb-primary:27017",
+            priority: 4
+        },
+        {
+            _id: 1,
+            host: "mongodb-rep1:27018",
+            priority: 3
+        },
+        {
+            _id: 2,
+            host: "mongodb-rep2:27019",
+            priority: 2
+        },
+        {
+            _id: 3,
+            host: "mongodb-rep3:27020",
+            priority: 1
         }
     ]
 });
-rs.add({
-    _id: 1,
-    host: "mongodb-rep1:27018"
-})
-rs.add({
-    _id: 2,
-    host: "mongodb-rep2:27019"
-})
-rs.add({
-    _id: 3,
-    host: "mongodb-rep3:27020"
-})
